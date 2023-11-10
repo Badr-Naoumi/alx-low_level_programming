cb38baefcc209a80cdaeef34f00dd55e42567038
Badr Naoumi <badr.naoumi11@gmail.com>
2023-11-10 15:36:03 -0800

    alx

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,11 +8,14 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL && action != NULL)
-	{
-	for (size_t i = 0; i < size; i++)
+	if (array == NULL || action == NULL)
+		return;
+
+	size_t i = 0;
+
+	while (i < size)
 	{
 		action(array[i]);
-	}
+		i++;
 	}
 }
